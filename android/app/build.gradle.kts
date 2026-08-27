fe88ba24d8001889b1040ac1b3088f520c8ecae3
1rachit-tech <kushwaharajkumaree@gmail.com>
2026-08-27 13:05:54 +0530

    Change application ID and namespace for Android app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.ojas_app"
+    namespace = "com.rachit.ojas"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.ojas_app"
+        applicationId = "com.rachit.ojas"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
