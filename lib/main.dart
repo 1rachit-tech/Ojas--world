ca399e58e53279978f92c0f40144a833290769b2
1rachit-tech <kushwaharajkumaree@gmail.com>
2026-08-27 09:51:19 +0000

    feat: add Firebase Core, Auth, Google Sign-In, and Firestore with latest versions

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const OjasApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const OjasApp());
+}
 
 class OjasApp extends StatelessWidget {
   const OjasApp({super.key});
