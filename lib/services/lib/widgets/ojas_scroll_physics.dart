8975008021541182d716197ce3582ff5f5651c02
Rachit Kushwaha <313784288+1rachit-tech@users.noreply.github.com>
2026-09-02 00:36:11 +0530

    Add custom OjasZeroJankScrollPhysics class

---
+++ lib/services/lib/widgets/ojas_scroll_physics.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class OjasZeroJankScrollPhysics extends BouncingScrollPhysics {
+  const OjasZeroJankScrollPhysics({super.parent});
+
+  @override
+  OjasZeroJankScrollPhysics applyTo(ScrollPhysics? ancestor) {
+    return OjasZeroJankScrollPhysics(parent: buildParent(ancestor));
+  }
+
+  @override
+  double get minFlingVelocity => 50.0; // Responsive touch on budget devices
+
+  @override
+  double get maxFlingVelocity => 8000.0;
+
+  @override
+  double frictionFactor(double overscrollFraction) => 0.65; // Silk-smooth 120fps snap
+}
