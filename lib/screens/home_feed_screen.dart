2f49159e2c8b133505a26a50b47e773db72d1e11
Rachit Kushwaha <313784288+1rachit-tech@users.noreply.github.com>
2026-09-16 10:51:12 +0530

    feat(home): expose recommendation controls from Home entry

--- lib/screens/home_feed_screen.dart
+++ lib/screens/home_feed_screen.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
-import 'home_feed_dynamic_screen.dart';
+import 'home_recommendation_overlay.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => const DynamicHomeScreen();
+  Widget build(BuildContext context) => const HomeRecommendationOverlay();
 }
 
 class HomeFeedScreen extends StatelessWidget {
   const HomeFeedScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => const DynamicHomeScreen();
+  Widget build(BuildContext context) => const HomeRecommendationOverlay();
 }
