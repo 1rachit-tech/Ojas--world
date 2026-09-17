591c414ea2f04f5684f4fecb4821d3f7c6c4c1b7
Rachit Kushwaha <313784288+1rachit-tech@users.noreply.github.com>
2026-09-17 10:59:29 +0530

    test: use stable post composer publish CTA assertion

--- test/creation_post_composer_test.dart
+++ test/creation_post_composer_test.dart
@@ -23,6 +23,6 @@ void main() {
     expect(find.text('Caption'), findsOneWidget);
     expect(find.text('Audience'), findsOneWidget);
     expect(find.text('Engagement & recommendations'), findsOneWidget);
-    expect(find.byIcon(Icons.publish_rounded), findsOneWidget);
+    expect(find.text('Publish to OJAS'), findsOneWidget);
   });
 }
